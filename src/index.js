6c734f88b41e51a9ca1649a2424602f228c83202
ivan <ifminier2@gmail.com>
2025-05-27 17:44:11 -0400

    Unregister service worker to fix stale manifest

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 👈 this is key
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+// 🔥 Disable service worker to clear old manifest
+serviceWorkerRegistration.unregister();
