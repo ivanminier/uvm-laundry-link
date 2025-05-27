351718b84a83920bf5927cda5f91ffc4009a5eed
ivan <ifminier2@gmail.com>
2025-05-27 00:35:10 -0400

    Sync package-lock, update PWA manifest & Tailwind/PostCSS configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
-  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        'uvm-green': '#154734',
-        'uvm-gold':  '#fdb515'
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
