f687db2caa9f8f765b537a30661c79a09799a0b5
ivan <ifminier2@gmail.com>
2025-06-01 13:56:07 -0400

    Update manifest.json for PWA support

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'laundrylink-static-v1';
+const URLsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(URLsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (
+    event.request.method !== 'GET' ||
+    new URL(event.request.url).origin !== location.origin
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then((networkResponse) => {
+          if (networkResponse.ok) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 👈 this is key
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-
-// 🔥 Disable service worker to clear old manifest
 serviceWorkerRegistration.register();
